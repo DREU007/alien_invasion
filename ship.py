1f0dd54785a94060ff738244e787f35f25ec04e4
DREU007 <dreu007@gmail.com>
2022-05-07 10:37:59 +0300

    Ship is flying left and right.

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
     """A class to manage ship."""
     
@@ -14,7 +15,18 @@ class Ship:
         
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
-        
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:    
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw the ship at ts current location."""
         self.screen.blit(self.image, self.rect)
