aa578f69e490790846083065cb8b5aaa0367b644
Denis Bolshakov <dreu007@gmail.com>
2023-04-24 10:43:18 +0400

    Add ai_game.py

---
+++ alien_invasion/scripts/ai_game.py
@@ -0,0 +1,11 @@
+from alien_invasion.engine import AlienInvasion
+
+
+def main():
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+if __name__ == '__main__':
+    main()
