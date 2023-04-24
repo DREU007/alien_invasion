3bd2d80b2c45ff2302e64d3d266cd5ef79c8c4ce
Denis Bolshakov <dreu007@gmail.com>
2023-04-24 10:06:01 +0400

    Update game_stats.py

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,9 +1,10 @@
+import os
 import json
 
 
 class GameStats:
     """Track statistics for Alien Invasion"""
-
+    _score_fp = os.path.join(os.path.dirname(__file__), 'high_score.json')
     def __init__(self, ai_game):
         """Initialize statistics"""
         self.settings = ai_game.settings
@@ -26,12 +27,12 @@ class GameStats:
 
     def load_high_score(self):
         """Load high_score.json as high_score"""
-        self.filename = 'high_score.json'
+        self.filename = self._score_fp
         with open(self.filename, 'r') as f:
             self.high_score = json.load(f)
 
     def save_high_score(self):
         """Save high_score as high_score.json"""
-        self.filename = 'high_score.json'
+        self.filename = self._score_fp
         with open(self.filename, 'w') as f:
             json.dump(self.high_score, f)
