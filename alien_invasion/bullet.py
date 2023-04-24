7e9fdce4498bf1bd9e4ac8ac5584363c42028e40
Denis Bolshakov <dreu007@gmail.com>
2023-04-24 09:50:08 +0400

    Fix bullet.py

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -14,10 +14,7 @@ class Bullet(Sprite):
 
         # Create a bullet rect at (0, 0) and then set correct position.
         self.rect = pygame.Rect(
-            left=0,
-            top=0,
-            width=self.settings.bullet_width,
-            height=self.settings.bullet_height
+            0, 0, self.settings.bullet_width, self.settings.bullet_height
         )
         self.rect.midtop = ai_game.ship.rect.midtop
 
